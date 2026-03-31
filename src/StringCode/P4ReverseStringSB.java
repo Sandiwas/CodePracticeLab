39d8aed3074eecdce35cb68725b53f21f697a208
sandiwas <harrycode9@gmail.com>
2026-03-31 12:44:57 +0530

    added revrese String using stringBuffer program

---
+++ src/StringCode/P4ReverseStringSB.java
@@ -0,0 +1,9 @@
+package StringCode;
+
+public class P4ReverseStringSB {
+	public static void main(String[] args) {
+		String str = "java";
+		StringBuffer sb = new StringBuffer(str);
+		System.out.print(sb.reverse());
+	}
+}
