cc13fd30060be369450d0de9570927d52dece6f2
sandiwas <harrycode9@gmail.com>
2026-03-31 13:02:58 +0530

    added Anagram swap string method

---
+++ src/StringCode/P8SwapWithoutUsingThirdVaribale.java
@@ -0,0 +1,16 @@
+package StringCode;
+
+public class P8SwapWithoutUsingThirdVaribale {
+	public static void main(String[] args) {
+		String s1 = "Hello";
+		String s2 = "World";
+		String s3;
+
+		s3 = s1;
+		s1 = s2;
+		s2 = s3;
+
+		System.out.println("s1 = " + s1);
+		System.out.println("s2 = " + s2);
+	}
+}
